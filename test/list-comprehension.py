25dcb0eee510fa66d10490c8fb80975fb59941a8
isteyak <isteyakislam12@gmail.com>
2023-12-28 23:41:59 -0500

    digital cv blueprints

--- test/list-comprehension.py
+++ test/list-comprehension.py
@@ -1,8 +1,10 @@
 dict = {0: 23, 1: 23, 2: 34}
 list1=[2,3,55]
-count=0
-for x, y in dict.items():
-    list1[count]=y
-    count+=1
+# count=0
+# for x, y in dict.items():
+#     list1[count]=y
+#     count+=1
     
+list1 = [x for x in dict.values()]
+
 print(list1)
